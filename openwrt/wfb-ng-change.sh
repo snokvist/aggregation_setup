ff07241f12492da9e45b641d821212e8bc4d82f2
snokvist <joakim.snokvist@gmail.com>
2025-03-10 16:48:11 +0100

    Update wfb-ng-change.sh

--- openwrt/wfb-ng-change.sh
+++ openwrt/wfb-ng-change.sh
@@ -7,25 +7,29 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Ensure three arguments are provided: <channel> <bandwidth> <region>
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <channel> <bandwidth> <region>"
+# Ensure four arguments are provided: <channel> <bandwidth> <region> <server_ip>
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 <channel> <bandwidth> <region> <server_ip>"
   exit 1
 fi
 
 CHANNEL=$1
 BANDWIDTH=$2
 REGION=$3
+SERVER_IP=$4
 
 echo "Updating default values in /usr/sbin/wfb-ng.sh to:"
 echo "  DEFAULT_CHANNEL=${CHANNEL}"
 echo "  DEFAULT_BANDWIDTH=${BANDWIDTH}"
 echo "  DEFAULT_REGION=${REGION}"
+echo "  DEFAULT_SERVER_IP=${SERVER_IP}"
+
 
 # Update the default values in /usr/sbin/wfb-ng.sh
 sed -i "s/^DEFAULT_CHANNEL=.*/DEFAULT_CHANNEL=${CHANNEL}/" /usr/sbin/wfb-ng.sh
 sed -i "s/^DEFAULT_BANDWIDTH=.*/DEFAULT_BANDWIDTH=\"${BANDWIDTH}\"/" /usr/sbin/wfb-ng.sh
 sed -i "s/^DEFAULT_REGION=.*/DEFAULT_REGION=\"${REGION}\"/" /usr/sbin/wfb-ng.sh
+sed -i "s/^DEFAULT_SERVER_IP=.*/SERVER_IP=\"${SERVER_IP}\"/" /usr/sbin/wfb-ng.sh
 
 echo "Defaults updated successfully."
 
