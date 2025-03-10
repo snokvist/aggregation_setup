b76b5f95c5ac051bc4b43ab94802a1628e0d33b4
snokvist <joakim.snokvist@gmail.com>
2025-03-10 16:00:50 +0100

    Create wfb-ng.sh

---
+++ openwrt/wfb-ng.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -emb
+
+export LC_ALL=C
+
+_cleanup()
+{
+  plist=$(jobs -p)
+  if [ -n "$plist" ]
+  then
+      kill -TERM $plist || true
+  fi
+  exit 1
+}
+
+trap _cleanup EXIT
+
+# Default values
+DEFAULT_CHANNEL=165
+DEFAULT_BANDWIDTH="HT20"
+DEFAULT_REGION="US"
+
+# If all three arguments are provided, override defaults
+if [ $# -eq 3 ]; then
+  CHANNEL=$1
+  BANDWIDTH=$2
+  REGION=$3
+else
+  CHANNEL=$DEFAULT_CHANNEL
+  BANDWIDTH=$DEFAULT_BANDWIDTH
+  REGION=$DEFAULT_REGION
+fi
+
+echo "Using channel: $CHANNEL"
+echo "Using bandwidth: $BANDWIDTH"
+echo "Using region: $REGION"
+
+# Set wireless region
+iw reg set "$REGION"
+
+# Read available WLAN interfaces from config file
+WFB_NICS=$(grep '^WFB_NICS=' /etc/default/wifibroadcast | cut -d'=' -f2 | tr -d '"')
+
+# Check if there are any interfaces
+if [ -z "$WFB_NICS" ]; then
+  echo "No WLAN interfaces found in /etc/default/wifibroadcast."
+  exit 1
+fi
+
+# Convert string to an array
+read -r -a WLAN_INTERFACES <<< "$WFB_NICS"
+
+for wlan in "${WLAN_INTERFACES[@]}"; do
+  echo "Initializing $wlan"
+
+  if which nmcli > /dev/null && ! nmcli device show "$wlan" | grep -q '(unmanaged)'; then
+    nmcli device set "$wlan" managed no
+    sleep 1
+  fi
+
+  ip link set "$wlan" down
+  iw dev "$wlan" set monitor otherbss
+  ip link set "$wlan" up
+  iw dev "$wlan" set channel "$CHANNEL" "$BANDWIDTH"
+done
+
+# gs_video
+wfb_rx -f -c 127.0.0.1 -u 10000 -p 0 -i 7669206 -R 2097152 "${WLAN_INTERFACES[@]}" &
+
+# gs_tunnel
+wfb_rx -f -c 127.0.0.1 -u 10001 -p 32 -i 7669206 -R 2097152 "${WLAN_INTERFACES[@]}" &
+wfb_tx -I 11001 -R 2097152 "${WLAN_INTERFACES[@]}" &
+
+echo "WFB-ng init done"
+wait -n
