414034e1115779f231120aba4eace1ba0f674f62
snokvist <joakim.snokvist@gmail.com>
2025-03-11 06:39:10 +0100

    Update sync_channel.sh

--- vtx/sync_channel.sh
+++ vtx/sync_channel.sh
@@ -2,8 +2,11 @@
 # Usage: sync_channel.sh <channel> <bandwidth> <region>
 # Note: <region> is accepted but ignored.
 set +e
-# Give the ssh shell some time to exit.
-sleep 0.1
+
+# Launch the killswitch in the background.
+# The killswitch will run for XX seconds, and if not killed before then,
+# it will restore the predetermined channel 165, HT20 settings and restart the service.
+killswitch.sh 165 HT20 &
 
 # Check for exactly 3 parameters
 if [ "$#" -ne 3 ]; then
@@ -15,20 +18,6 @@ CHANNEL="$1"
 BANDWIDTH="$2"
 # REGION is provided but will be silently ignored
 REGION="$3"
-
-# Get the current channel value from the YAML file
-CURRENT_CHANNEL=$(yaml-cli -i /etc/wfb.yaml -g .wireless.channel 2>/dev/null)
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to get current wireless channel from /etc/wfb.yaml" >&2
-  exit 1
-fi
-
-# Get the current wifi_mode from the YAML file
-CURRENT_WIFI_MODE=$(yaml-cli -i /etc/wfb.yaml -g .wireless.wifi_mode 2>/dev/null)
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to get current wifi_mode from /etc/wfb.yaml" >&2
-  exit 1
-fi
 
 # Set the new channel value using the provided <channel>
 yaml-cli -i /etc/wfb.yaml -s .wireless.channel "$CHANNEL" 2>/dev/null
@@ -64,11 +53,6 @@ fi
 
 # Restart the majestic service in case it died
 /etc/init.d/S95majestic restart
-
-# Launch the killswitch in the background.
-# The killswitch will run for 10 seconds, and if not killed before then,
-# it will restore the original settings and restart the service.
-killswitch.sh "$CURRENT_CHANNEL" "$CURRENT_WIFI_MODE" &
 
 # Output success message with the new settings in one row
 echo "Success: channel set to $CHANNEL, wifi_mode set to $BANDWIDTH"
